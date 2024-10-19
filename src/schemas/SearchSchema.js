39b2b80ae6f47f94d790e87e01de2759951207b7
Adam Teodoro <66885644+AdamTeodoro@users.noreply.github.com>
2024-10-19 01:44:44 +0000

    Update SearchSchema.js
    change indentation

--- src/schemas/SearchSchema.js
+++ src/schemas/SearchSchema.js
@@ -2,29 +2,29 @@ import Joi from "joi"
 
 export const SearchSchema =  Joi.object({
     brand: Joi.string()
-    .min(1)
-    .max(256)
-    .required(),
+        .min(1)
+        .max(256)
+        .required(),
 
     color: Joi.string()
-    .min(1)
-    .max(256)
-    .trim()
-    .required(),
+        .min(1)
+        .max(256)
+        .trim()
+        .required(),
     
     year: Joi.string()
-    .min(3)
-    .max(256)
-    .trim()
-    .required(),
+        .min(3)
+        .max(256)
+        .trim()
+        .required(),
 
     minPrice: Joi.number()
-    .min(0)
-    .max(9999999999999)
-    .required(),
+        .min(0)
+        .max(9999999999999)
+        .required(),
 
     maxPrice: Joi.number()
-    .min(0)
-    .max(9999999999999)
-    .required(),
+        .min(0)
+        .max(9999999999999)
+        .required(),
 }).or('brand', 'name', 'color', 'year').with('minPrice', 'maxPrice');
